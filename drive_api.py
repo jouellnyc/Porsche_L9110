8777003fe896fcf548c743b2c60e619fc9b3b795
John Ouellette <jouellnyc@gmail.com>
2024-09-29 13:05:24 -0400

    Correct right and left turns

--- drive_api.py
+++ drive_api.py
@@ -55,59 +55,59 @@ class CarControl:
         
         # Initial speed (0-65535)
         self.speed = speed
-        
-        
-    def move_forward(self):
-        """Move the car forward"""
-        self.Lforward.duty_u16(self.speed)
-        self.Rforward.duty_u16(self.speed)
+    
+    def stop_back_wheels(self):
         self.Lbackward.duty_u16(0)
         self.Rbackward.duty_u16(0)
         
-    def move_backward(self):
-        """Move the car backward"""
+    def stop_front_wheels(self):
         self.Lforward.duty_u16(0)
         self.Rforward.duty_u16(0)
+        
+    def move_forward(self):
+        """Move the car forward"""
+        self.stop_back_wheels()
+        self.Lforward.duty_u16(self.speed)
+        self.Rforward.duty_u16(self.speed)
+        
+    def move_backward(self):
+        """Move the car backward"""
+        self.stop_front_wheels()
         self.Lbackward.duty_u16(self.speed)
         self.Rbackward.duty_u16(self.speed)
         
     def stop(self):
         """Stop the car"""
-        self.Lforward.duty_u16(0)
-        self.Rforward.duty_u16(0)
-        self.Lbackward.duty_u16(0)
-        self.Rbackward.duty_u16(0)
+        self.stop_front_wheels()
+        self.stop_back_wheels()
         
     def turn_left(self, turn_rate):
         """
-        Turn left by reducing the right wheel speed
+        Turn left by reducing the left wheel speed
         turn_rate: 0-100 (percentage to reduce), max to 50%
-        
         ###jjo don't let it slip below 50
         """
-        self.right_speed = max(35535, int(self.speed * ((100 - turn_rate) / 100)))
-        self.left_speed   = self.speed
-        
-        self.Rforward.duty_u16(self.right_speed)
-        self.Lforward.duty_u16(self.left_speed)
-        
-        self.Lbackward.duty_u16(0)
-        self.Rbackward.duty_u16(0)
-        print("LS, RS", self.left_speed, self.right_speed)
-        
-    def turn_right(self, turn_rate):
-        """
-        Turn right by reducing the left wheel speed
-        turn_rate: 0-100 (percentage to reduce right wheel speed)
-        """
-        self.left_speed = max(35535, int(self.speed * ((100 - turn_rate) / 100)))
+        self.left_speed = max(45535, int(self.speed * ((100 - turn_rate) / 100)))
         self.right_speed   = self.speed
         
         self.Rforward.duty_u16(self.right_speed)
         self.Lforward.duty_u16(self.left_speed)
         
-        self.Lbackward.duty_u16(0)
-        self.Rbackward.duty_u16(0)
+        self.stop_back_wheels()
         print("LS, RS", self.left_speed, self.right_speed)
         
+    def turn_right(self, turn_rate):
+        """
+        Turn right by reducing the right wheel speed
+        turn_rate: 0-100 (percentage to reduce right wheel speed)
+        """
+        self.right_speed = max(45535, int(self.speed * ((100 - turn_rate) / 100)))
+        self.left_speed   = self.speed
         
+        self.Rforward.duty_u16(self.right_speed)
+        self.Lforward.duty_u16(self.left_speed)
+        self.stop_back_wheels()
+        
+        print("LS, RS", self.left_speed, self.right_speed)        
+    
+    
--- infra_car_control.py
+++ infra_car_control.py
@@ -5,7 +5,7 @@ percent=1
 car = CarControl(speed=int(65535 * percent), freq=1000)
 
 #The 'OK' Button and 1, 2, 3
-pri_scan_codes=[28, 69, 70, 71]
+pri_scan_codes=[28, 69, 70, 71, 64, 68]
 
 def infrared_check(data):
     if data in pri_scan_codes:
@@ -16,6 +16,10 @@ def infrared_check(data):
         elif data == 70:
             car.stop()
             car.move_backward()
+        elif data == 64:
+            car.turn_right(50)
+        elif data == 68:
+            car.turn_left(50)
         elif data == 71:
             car.stop()
             car.turn_left(50)
@@ -23,8 +27,7 @@ def infrared_check(data):
             car.turn_right(50) 
         elif data == 28:
             car.stop()
-            
     else:
-        print('NOPE', data)
+        print('Not in pri_scan_codes', data)
                            
                 
